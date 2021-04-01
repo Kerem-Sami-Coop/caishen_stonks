f775145965421e1d1b1b13d243ab61ef7b6c2fc7
sarodz <samirodrigue@hotmail.com>
2021-04-01 14:54:20 -0400

    smaller request

--- tests/utils/test_stockdata.py
+++ tests/utils/test_stockdata.py
@@ -2,7 +2,7 @@ from caishen_dashboard.utils.stock import StockData
 
 
 def test_stock_data():
-    init = StockData(stock="AAPL", start="20190101", end="20200101", output_format="pandas")
+    init = StockData(stock="AAPL", start="20190101", end="20190103", output_format="pandas")
     data = init.retrieveData()
     shape = data.shape
 
