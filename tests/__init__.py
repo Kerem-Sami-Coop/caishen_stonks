d1e34b82a892cf0d013ae477f0dff83753250379
sarodz <samirodrigue@hotmail.com>
2021-04-08 19:36:12 -0400

    update init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 import sys
 from pathlib import Path
 import os
+import yaml
+
 
 # Pytest has trouble identifying some imports without updating the
 # python Path to include the app and tests modules
@@ -9,13 +11,12 @@ sys.path.insert(0, f"{service_root}")
 sys.path.insert(0, f"{service_root}/src")
 sys.path.insert(0, f"{service_root}/tests")
 
-if os.path.exists("secrets.yaml"):
-    import yaml
-    with open("secrets.yaml", "r") as f:
-        yaml_config = yaml.safe_load(f)
+with open("dev_env_var.yaml", "r") as f:
+    yaml_config = yaml.safe_load(f)
 
-    with open("dev_env_var.yaml", "r") as f:
+if os.path.exists("secrets.yaml"):
+    with open("secrets.yaml", "r") as f:
         yaml_config.update(yaml.safe_load(f))
 
-    for key in yaml_config.keys():
-        os.environ[key] = yaml_config[key]
+for key in yaml_config.keys():
+    os.environ[key] = yaml_config[key]
